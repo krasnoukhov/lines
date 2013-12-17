cfdfdb8bd00c9bc30dae2137c3873425d817f5a2
andreyk6 <andreyk6@gmail.com>
2013-12-17 20:55:53 +0200

    Update andreyk6.cpp

--- solutions/andreyk6.cpp
+++ solutions/andreyk6.cpp
@@ -5,216 +5,238 @@
 using namespace std;
 
 int DetectLine(int field[100][100],int n){
-        int pos,tempLength,maxLength=0,tempColor,i,j=0,totalLines=0;
+	int pos,tempLength,maxLength=0,tempColor,i,j=0,totalLines=0;
 
-        for (i=0;i<n;i++){
-                //Поиск самой длинной линии по горизонтали                
-                maxLength=0;
-                tempLength=0;
-                tempColor=0;
-                for (j=0;j<n;j++){
-                        if (tempColor==0 && field[i][j]!=0){
-                                tempColor=field[i][j];
-                                tempLength=1;
-                        } else if(tempColor==field[i][j] && tempColor!=0){
-                                tempLength++;
-                                if(maxLength<tempLength) maxLength=tempLength;
-                        } else if(tempColor!=field[i][j] && tempColor!=0){
-                                tempColor=field[i][j];
-                                tempLength=1;
-                        }
-                }
-                
-                if(maxLength>=5) totalLines++;
+	for (i=0;i<n;i++){
+		//Поиск самой длинной линии по горизонтали                
+		maxLength=0;
+		tempLength=0;
+		tempColor=0;
+		for (j=0;j<n;j++){
+			if (tempColor==0 && field[i][j]!=0){
+				tempColor=field[i][j];
+				tempLength=1;
+			} else if(tempColor==field[i][j] && tempColor!=0){
+				tempLength++;
+				if(maxLength<tempLength) maxLength=tempLength;
+			} else if(tempColor!=field[i][j] && tempColor!=0){
+				tempColor=field[i][j];
+				tempLength=1;
+			}
+		}
 
-                //Поиск самой длинной линии по вертикали
-                maxLength=0;
-                tempLength=0;
-                tempColor=0;
-                for (j=0;j<n;j++){
-                        if (tempColor==0 && field[j][i]!=0){
-                                tempColor=field[j][i];
-                                tempLength=1;
-                        } else if(tempColor==field[j][i] && tempColor!=0){
-                                tempLength++;
-                                if(maxLength<tempLength && tempColor!=0) maxLength=tempLength;
-                        } else if(tempColor!=field[j][i]){
-                                tempColor=field[j][i];
-                                tempLength=1;
-                        }
-                }
+		if(maxLength>=5) totalLines++;
 
-                if(maxLength>=5) totalLines++;
+		//Поиск самой длинной линии по вертикали
+		maxLength=0;
+		tempLength=0;
+		tempColor=0;
+		for (j=0;j<n;j++){
+			if (tempColor==0 && field[j][i]!=0){
+				tempColor=field[j][i];
+				tempLength=1;
+			} else if(tempColor==field[j][i] && tempColor!=0){
+				tempLength++;
+				if(maxLength<tempLength && tempColor!=0) maxLength=tempLength;
+			} else if(tempColor!=field[j][i]){
+				tempColor=field[j][i];
+				tempLength=1;
+			}
+		}
 
-        }
-        
+		if(maxLength>=5) totalLines++;
 
-        //Поиск в диагонали
-        for (i=0;i<n;i++){
-                //Поиск самой длинной линии по горизонтали               
-                pos=0;
-                maxLength=0;
-                tempLength=0;
-                tempColor=0;
-                for (j=i;j>0,pos<=i;j--){
-                        if (tempColor==0 && field[pos][j]!=0){
-                                tempColor=field[pos][j];
-                                tempLength=1;
-                        } else if(tempColor==field[pos][j] && tempColor!=0){
-                                tempLength++;
-                                if(maxLength<tempLength) maxLength=tempLength;
-                        } else if(tempColor!=field[pos][j] && tempColor!=0){
-                                tempColor=field[pos][j];
-                                tempLength=1;
-                        }
-                        pos++;
-                }
-                
-                if(maxLength>=5) 
-					totalLines++;
+	}
 
-				//Проверяем нижнюю часть
-				pos=n;
-                maxLength=0;
-                tempLength=0;
-                tempColor=0;
-                for (j=i;j<n,i!=0,pos>=0;j++){
-                        if (tempColor==0 && field[pos][j]!=0){
-                                tempColor=field[pos][j];
-                                tempLength=1;
-                        } else if(tempColor==field[pos][j] && tempColor!=0){
-                                tempLength++;
-                                if(maxLength<tempLength) maxLength=tempLength;
-                        } else if(tempColor!=field[pos][j] && tempColor!=0){
-                                tempColor=field[pos][j];
-                                tempLength=1;
-                        }
-                        pos--;
-                }
-                
-                if(maxLength>=5) 
-					totalLines++;
-                
-                pos=0;
-                maxLength=0;
-                tempLength=0;
-                tempColor=0;
-                for (j=0;j<n,pos<=i;j++){
-					if (tempColor==0 && field[pos][j]!=0){
-						tempColor=field[pos][j];
-                                tempLength=1;
-                        } else if(tempColor==field[pos][j] && tempColor!=0){
-                               tempLength++;
-                                if(maxLength<tempLength) maxLength=tempLength;
-					} else if(tempColor!=field[pos][j] && tempColor!=0){
-                                tempColor=field[pos][j];
-                                tempLength=1;
-                        }
-                        pos++;
-                }
-                
-                if(maxLength>=5) 
-					totalLines++;
-        }
-        
-        return totalLines;
+
+	//Поиск последовательности в диагоналях
+	for (i=0;i<n;i++){
+		//Поиск верхней части ( направление //// )            
+		pos=0;
+		maxLength=0;
+		tempLength=0;
+		tempColor=0;
+		for (j=i;j>0,pos<=i;j--){
+			if (tempColor==0 && field[pos][j]!=0){
+				tempColor=field[pos][j];
+				tempLength=1;
+			} else if(tempColor==field[pos][j] && tempColor!=0){
+				tempLength++;
+				if(maxLength<tempLength) maxLength=tempLength;
+			} else if(tempColor!=field[pos][j] && tempColor!=0){
+				tempColor=field[pos][j];
+				tempLength=1;
+			}
+			pos++;
+		}
+
+		if(maxLength>=5) 
+			totalLines++;
+
+		//Проверяем нижнюю часть ( направление //// )
+		pos=n;
+		maxLength=0;
+		tempLength=0;
+		tempColor=0;
+		for (j=i;j<n,i!=0,pos>=0;j++){
+			if (tempColor==0 && field[pos][j]!=0){
+				tempColor=field[pos][j];
+				tempLength=1;
+			} else if(tempColor==field[pos][j] && tempColor!=0){
+				tempLength++;
+				if(maxLength<tempLength) maxLength=tempLength;
+			} else if(tempColor!=field[pos][j] && tempColor!=0){
+				tempColor=field[pos][j];
+				tempLength=1;
+			}
+			pos--;
+		}
+
+		if(maxLength>=5) 
+			totalLines++;
+
+		//Поиск в верхней части ( направление \\\\\ )
+		pos=i;
+		maxLength=0;
+		tempLength=0;
+		tempColor=0;
+		for (j=0;j<n,pos+j<n;j++){
+			if (tempColor==0 && field[j][pos+j]!=0){
+				tempColor=field[j][pos+j];
+				tempLength=1;
+			} else if(tempColor==field[j][pos+j] && tempColor!=0){
+				tempLength++;
+				if(maxLength<tempLength) maxLength=tempLength;
+			} else if(tempColor!=field[j][pos+j] && tempColor!=0){
+				tempColor=field[j][pos+j];
+				tempLength=1;
+			}
+		}
+
+		if(maxLength>=5) 
+			totalLines++;
+
+
+		//Поиск в нижней части ( направление \\\\\ )
+		pos=i;
+		maxLength=0;
+		tempLength=0;
+		tempColor=0;
+		for (j=0;j<n,pos+j<n,i!=0;j++){
+			if (tempColor==0 && field[pos+j][j]!=0){
+				tempColor=field[pos+j][j];
+				tempLength=1;
+			} else if(tempColor==field[pos+j][j] && tempColor!=0){
+				tempLength++;
+				if(maxLength<tempLength) maxLength=tempLength;
+			} else if(tempColor!=field[pos+j][j] && tempColor!=0){
+				tempColor=field[pos+j][j];
+				tempLength=1;
+			}
+		}
+
+		if(maxLength>=5) 
+			totalLines++;
+	}
+
+	return totalLines;
 }
 
 int ReadAndMove(int field[100][100],int n){
-        int x,y,x2,y2,result;
-        string strParam,strData,strColor,strTemp;
+	int x,y,x2,y2,result;
+	string strParam,strData,strColor,strTemp;
 
-        //Читаем начальные параметры
-        cin >> strParam;
-        cin >> strData;
-        while(strParam=="BALL"){
-                //Оторвали X
-                strTemp=strData.substr(0,strData.find(","));
-                x=atoi(strTemp.c_str());
+	//Читаем начальные параметры
+	cin >> strParam;
+	cin >> strData;
+	while(strParam=="BALL"){
+		//Оторвали X
+		strTemp=strData.substr(0,strData.find(","));
+		x=atoi(strTemp.c_str());
 
-                //Отрезали лишние данные
-                strData=strData.substr(strData.find(",")+1,strData.length());
+		//Отрезали лишние данные
+		strData=strData.substr(strData.find(",")+1,strData.length());
 
-                //Оторвали Y
-                strTemp=strData.substr(0,strData.find(","));
-                y=atoi(strTemp.c_str());
+		//Оторвали Y
+		strTemp=strData.substr(0,strData.find(","));
+		y=atoi(strTemp.c_str());
 
-                //Достали цвет
-                strColor=strData.substr(strData.find(",")+1,strData.length());
+		//Достали цвет
+		strColor=strData.substr(strData.find(",")+1,strData.length());
 
-                if(x>=0 && x<n && y>=0 && y<n)
-                        if (strColor=="red") field[y][x]=1; else
-                                if (strColor=="green") field[y][x]=2; else
-                                        if (strColor=="blue") field[y][x]=3; else {
-										//	return 0;
-										}
-                cin >> strParam;
-                cin >> strData;
+		if(x>=0 && x<n && y>=0 && y<n)
+			if (strColor=="red") field[y][x]=1; else
+				if (strColor=="green") field[y][x]=2; else
+					if (strColor=="blue") field[y][x]=3; else {
+						//	return 0;
+					}
+					cin >> strParam;
+					cin >> strData;
 
-        }
+	}
 
-        //Двигаем шарики
+	//Двигаем шарики
 
-        while(strParam=="MOVE"){
-                //Оторвали X
-                strTemp=strData.substr(0,strData.find(","));
-                x=atoi(strTemp.c_str());
+	while(strParam=="MOVE"){
+		//Оторвали X
+		strTemp=strData.substr(0,strData.find(","));
+		x=atoi(strTemp.c_str());
 
-                //Отрезали лишние данные
-                strData=strData.substr(strData.find(",")+1,strData.length());
+		//Отрезали лишние данные
+		strData=strData.substr(strData.find(",")+1,strData.length());
 
-                //Оторвали Y
-                strTemp=strData.substr(0,strData.find(","));
-                y=atoi(strTemp.c_str());
+		//Оторвали Y
+		strTemp=strData.substr(0,strData.find(","));
+		y=atoi(strTemp.c_str());
 
-                //Отрезали лишние данные
-                strData=strData.substr(strData.find(",")+1,strData.length());
+		//Отрезали лишние данные
+		strData=strData.substr(strData.find(",")+1,strData.length());
 
-                //Оторвали X2
-                strTemp=strData.substr(0,strData.find(","));
-                x2=atoi(strTemp.c_str());
+		//Оторвали X2
+		strTemp=strData.substr(0,strData.find(","));
+		x2=atoi(strTemp.c_str());
 
-                //Оторвали Y2
-                strTemp=strData.substr(strData.find(",")+1,strData.length());
-                y2=atoi(strTemp.c_str());
+		//Оторвали Y2
+		strTemp=strData.substr(strData.find(",")+1,strData.length());
+		y2=atoi(strTemp.c_str());
 
-				if(0<=x<n && 0<=y<n && 0<=x2<n && 0<=y2<n && 0<=abs(x2-x)<=1 && 0<=abs(y2-y)<=1) 
-                        if(field[y2][x2]==0 && field[y][x]!=0){
-                                field[y2][x2]=field[y][x];
-                                field[y][x]=0;
-                        } //else 
-                        //return 0;
+		if(0<=x<n && 0<=y<n && 0<=x2<n && 0<=y2<n && 0<=abs(x2-x)<=1 && 0<=abs(y2-y)<=1) 
+			if(field[y2][x2]==0 && field[y][x]!=0){
+				field[y2][x2]=field[y][x];
+				field[y][x]=0;
+			} else { 
+				//	return 0;
+			}
 
-                
-                if (cin.eof()) break;
-                cin >> strParam;
-                cin >> strData;
-        }
+			if (cin.eof()) break;
+			cin >> strParam;
+			cin >> strData;
+	}
 
-        return DetectLine(field,n);
+	return DetectLine(field,n);
 } 
 
 int main(){
-        int n; 
-        string strParam;
-        
-//       freopen("input.txt","r",stdin);
-//      freopen("output.txt","w",stdout);
+	int n; 
+	string strParam;
 
-        //Читаем размер
-        cin >> strParam >> n;
-		int field[100][100]={0};
-        //Создали массив
-//                int **field = new int *[n];
-//                for (int i=0;i<n;i++) 
-//                 field[i]=new int[n];
-        for (int i=0;i<n;i++)
-                for(int j=0;j<n;j++)
-                        field[i][j]=0;
+//	freopen("input.txt","r",stdin);
+//	freopen("output.txt","w",stdout);
 
-        //Заполняем массив входными данными и двигаем шарики
-        cout << ReadAndMove(field,n);
+	//Читаем размер
+	cin >> strParam >> n;
+	int field[100][100]={0};
+	//Создали массив
+	//                int **field = new int *[n];
+	//                for (int i=0;i<n;i++) 
+	//                 field[i]=new int[n];
+	for (int i=0;i<n;i++)
+		for(int j=0;j<n;j++)
+			field[i][j]=0;
 
-      //  delete []field;
-        return 0;
+	//Заполняем массив входными данными и двигаем шарики
+	cout << ReadAndMove(field,n);
+
+	//  delete []field;
+	return 0;
 }

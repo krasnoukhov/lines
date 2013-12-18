bb2ce4b87d200c8996a5801f56055dce46d1d4a4
andreyk6 <andreyk6@gmail.com>
2013-12-18 16:52:30 +0200

    Update andreyk6.cpp

--- solutions/andreyk6.cpp
+++ solutions/andreyk6.cpp
@@ -5,10 +5,10 @@
 using namespace std;
 
 int DetectLine(int **&field,int n){
-    int pos,tempLength,maxLength=0,tempColor,i,j=0,totalLines=0;
+	int pos,tempLength,maxLength=0,tempColor,i,j=0,totalLines=0;
 
-    for (i=0;i<n;i++){
-    	//Поиск самой длинной линии по горизонтали                
+	for (i=0;i<n;i++){
+		//Поиск самой длинной линии по горизонтали                
 		maxLength=0;
 		tempLength=0;
 		tempColor=field[i][0];
@@ -149,75 +149,67 @@ int ReadAndMove(int **&field,int n){
 	string strParam,strData,strColor,strTemp;
 
 	//Читаем начальные параметры
-	cin >> strParam;
-	cin >> strData;
-	while(strParam=="BALL"){
-		//Оторвали X
-		strTemp=strData.substr(0,strData.find(","));
-		x=atoi(strTemp.c_str());
+	//cin >> strParam;
+	//cin >> strData;
+	while(cin >> strParam && cin >> strData){
+		if(strParam=="BALL"){
+			//Оторвали X
+			strTemp=strData.substr(0,strData.find(","));
+			x=atoi(strTemp.c_str());
 
-		//Отрезали лишние данные
-		strData=strData.substr(strData.find(",")+1,strData.length());
+			//Отрезали лишние данные
+			strData=strData.substr(strData.find(",")+1,strData.length());
 
-		//Оторвали Y
-		strTemp=strData.substr(0,strData.find(","));
-		y=atoi(strTemp.c_str());
+			//Оторвали Y
+			strTemp=strData.substr(0,strData.find(","));
+			y=atoi(strTemp.c_str());
 
-		//Достали цвет
-		strColor=strData.substr(strData.find(",")+1,strData.length());
+			//Достали цвет
+			strColor=strData.substr(strData.find(",")+1,strData.length());
 
-		if(x>=0 && x<n && y>=0 && y<n)
-			if (strColor=="red") field[y][x]=1; else
-				if (strColor=="green") field[y][x]=2; else
-					if (strColor=="blue") field[y][x]=3;
-                    
-					cin >> strParam;
-					cin >> strData;
+			if(x>=0 && x<n && y>=0 && y<n)
+				if (strColor=="red") field[y][x]=1; else
+					if (strColor=="green") field[y][x]=2; else
+						if (strColor=="blue") field[y][x]=3;
+		}
+		if (strParam=="MOVE"){
+			//Оторвали X
+			strTemp=strData.substr(0,strData.find(","));
+			x=atoi(strTemp.c_str());
 
-	}
+			//Отрезали лишние данные
+			strData=strData.substr(strData.find(",")+1,strData.length());
 
-	//Двигаем шарики
+			//Оторвали Y
+			strTemp=strData.substr(0,strData.find(","));
+			y=atoi(strTemp.c_str());
 
-	while(strParam=="MOVE"){
-		//Оторвали X
-		strTemp=strData.substr(0,strData.find(","));
-		x=atoi(strTemp.c_str());
+			//Отрезали лишние данные
+			strData=strData.substr(strData.find(",")+1,strData.length());
 
-		//Отрезали лишние данные
-		strData=strData.substr(strData.find(",")+1,strData.length());
+			//Оторвали X2
+			strTemp=strData.substr(0,strData.find(","));
+			x2=atoi(strTemp.c_str());
 
-		//Оторвали Y
-		strTemp=strData.substr(0,strData.find(","));
-		y=atoi(strTemp.c_str());
+			//Оторвали Y2
+			strTemp=strData.substr(strData.find(",")+1,strData.length());
+			y2=atoi(strTemp.c_str());
 
-		//Отрезали лишние данные
-		strData=strData.substr(strData.find(",")+1,strData.length());
 
-		//Оторвали X2
-		strTemp=strData.substr(0,strData.find(","));
-		x2=atoi(strTemp.c_str());
 
-		//Оторвали Y2
-		strTemp=strData.substr(strData.find(",")+1,strData.length());
-		y2=atoi(strTemp.c_str());
+			if(x>=0 && x<n && y>=0 && y<n && x2>=0 && x2<n && y2>=0 && y2<n && abs(x2-x)>=0 && abs(x2-x)<=1 && abs(y2-y)<=1 && abs(y2-y)>=0) 
+				if(field[y2][x2]==0 && field[y][x]!=0){
+					field[y2][x2]=field[y][x];
+					field[y][x]=0;
+				} else { 
+					//        return 0;
+				}
+				x=n+100;
+				y=n+100;
+				x2=-n-100;
+				y2=-n-100;
+		}
 
-	
-
-		if(x>=0 && x<n && y>=0 && y<n && x2>=0 && x2<n && y2>=0 && y2<n && abs(x2-x)>=0 && abs(x2-x)<=1 && abs(y2-y)<=1 && abs(y2-y)>=0) 
-			if(field[y2][x2]==0 && field[y][x]!=0){
-				field[y2][x2]=field[y][x];
-				field[y][x]=0;
-			} else { 
-				//	return 0;
-			}
-		if ( !feof( stdin )) break;
-		x=n+100;
-        	y=n+100;
-        	x2=-n-100;
-        	y2=-n-100;
-        		
-		cin >> strParam;
-		cin >> strData;
 	}
 
 	return DetectLine(field,n);
@@ -227,15 +219,15 @@ int main(){
 	int n; 
 	string strParam;
 
-//	freopen("input.txt","r",stdin);
-//	freopen("output.txt","w",stdout);
+	freopen("input.txt","r",stdin);
+	//        freopen("output.txt","w",stdout);
 
 	//Читаем размер
 	cin >> strParam >> n;
-//	int field[100][100]={0};
+	//        int field[100][100]={0};
 	//Создали массив
 	int **field = new int *[n];
-	
+
 	for (int i=0;i<n;i++) 
 		field[i]=new int[n];
 	for (int i=0;i<n;i++)
